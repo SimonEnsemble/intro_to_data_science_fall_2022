b133c3ce361b19af8860d1a8c801cafc58a8846d
SimonEnsemble <cory.simon@oregonstate.edu>
2022-09-20 03:46:20 +0000

    Deploying to gh-pages from  @ 31088953edf03efd4da9cade2c7d2f0d81605743 🚀

--- lectures/complete/working_with_data.jl
+++ lectures/complete/working_with_data.jl
@@ -36,7 +36,7 @@ md"## simple operations on `DataFrames`
 
 ### construct from scratch
 
-construct a `DataFrame`, `cities_data`, that contains information about cities in the US.
+construct a `DataFrame`, `city_data`, that contains information about cities in the US.
 
 * rows = instances = cities
 * columns = features = attributes of cities
@@ -49,14 +49,14 @@ city = ["Corvallis", "Portland", "Eugene"]
 population = [57961, 647805, 168916]
 
 # ╔═╡ 2071b5b6-038f-11eb-182d-f392b2198f2e
-# cities_data = DataFrame(city=city, population=population)
-cities_data = DataFrame(city=["Corvallis", "Portland", "Eugene"],
+# city_data = DataFrame(city=city, population=population)
+city_data = DataFrame(city=["Corvallis", "Portland", "Eugene"],
 	                    population=[57961, 647805, 168916])
 
 # ╔═╡ bc58ac1c-033c-11eb-2698-f5fc4c20b8ce
 md"### append rows
 
-append two new rows to `cities_data`:
+append two new rows to `city_data`:
 * Bend, OR has a population of 94520.
 * Berkeley, CA has a population of 122324.
 
@@ -66,7 +66,7 @@ _approach 1_: think of rows of a `DataFrame` as an `Array`.
 # ╔═╡ d172a490-033c-11eb-157e-6b95587099dd
 begin
 	new_row = ["Bend", 94520]
-	push!(cities_data, new_row)
+	push!(city_data, new_row)
 end
 
 # ╔═╡ 78b1f924-033d-11eb-2937-ff9634f5aa9a
@@ -77,13 +77,13 @@ _approach 2_: think of rows of a `DataFrame` as a `Dict`.
 # ╔═╡ 4c407c9a-0353-11eb-0618-955711917f54
 begin
 	another_new_row = Dict("population" => 122324, "city" => "Berkeley")
-	push!(cities_data, another_new_row)
+	push!(city_data, another_new_row)
 end
 
 # ╔═╡ 5e293c82-033d-11eb-3984-7164bf9a351d
 md"### append columns
 
-append another two column to `cities_data` giving the (1) annual rainfall in each city and (2) state to which the city belongs.
+append another two column to `city_data` giving the (1) annual rainfall in each city and (2) state to which the city belongs.
 * Corvallis, OR: 51.0 in
 * Portland, OR: 43.0 in
 * Eugene, OR: 47.0 in
@@ -97,40 +97,40 @@ append another two column to `cities_data` giving the (1) annual rainfall in eac
 "
 
 # ╔═╡ 400d2f1a-5033-4581-a5b4-f57f101bef4e
-cities_data[:, "rain"] = [51.0, 43.0, 47.0, 12.0, 25.0]
+city_data[:, "rain"] = [51.0, 43.0, 47.0, 12.0, 25.0]
 
 # ╔═╡ 452fee83-985a-4eab-9c9d-2223645056f0
-cities_data
+city_data
 
 # ╔═╡ 014a4866-a14d-444d-81cb-5c3e242f855d
 md"*approach 2:* employ the `insertcols!` function, which allows you to insert a column at a specified location."
 
 # ╔═╡ a5e9fc00-0353-11eb-1443-63b1c2edab7c
-insertcols!(cities_data, 2, :state => ["OR", "OR", "OR", "OR", "CA"])
+insertcols!(city_data, 2, :state => ["OR", "OR", "OR", "OR", "CA"])
 
 # ╔═╡ c94b33a0-3023-45fb-9c77-354579d742fb
 md"*approach 3:* the `transform` function, which transforms current column(s) into a new one. see [the docs](https://dataframes.juliadata.org/stable/lib/functions/#DataFrames.transform!)."
 
 # ╔═╡ 46f095d4-630a-4661-bb37-a0084d11b6b6
-transform!(cities_data, "rain" => (col -> 2 * col) => "twice_the_rainfall")
+transform!(city_data, "rain" => (col -> 2 * col) => "twice_the_rainfall")
 
 # ╔═╡ a67b30b0-0353-11eb-2d2f-871d7a5ffd36
 md"### count the # of rows/columns"
 
 # ╔═╡ 6249187e-035a-11eb-2f6a-d3318cf2a996
-size(cities_data) # (# rows, # cols)
+size(city_data) # (# rows, # cols)
 
 # ╔═╡ 6f8ca3cc-5935-4abe-b628-ac3c8cf4cd97
-nrow(cities_data)
+nrow(city_data)
 
 # ╔═╡ 84f3f188-b90c-4fcf-994b-5d32dd0551d5
-ncol(cities_data)
+ncol(city_data)
 
 # ╔═╡ 9b63b70a-7ea6-48a6-a068-602de654681d
 md"### retreive the names of the columns"
 
 # ╔═╡ f2cbf254-69fc-4e9f-974d-468ce53040a8
-names(cities_data)
+names(city_data)
 
 # ╔═╡ a3421e44-035e-11eb-3cf7-c70142f0591d
 md"
@@ -138,7 +138,7 @@ md"
 "
 
 # ╔═╡ a9d20a30-035e-11eb-14f4-ddf7cdaa34f6
-rename!(cities_data, "rain" => "rainfall")
+rename!(city_data, "rain" => "rainfall")
 
 # ╔═╡ 63716d2a-0362-11eb-3ce5-3b41d4bef04c
 md"
@@ -148,20 +148,20 @@ use the `select!` function. see [the docs](https://dataframes.juliadata.org/stab
 "
 
 # ╔═╡ 5931d59e-0391-11eb-078b-ddb0bcaf6521
-select!(cities_data, Not("twice_the_rainfall"))
+select!(city_data, Not("twice_the_rainfall"))
 
 # ╔═╡ 48d7d749-c7d0-407c-8863-8ab7530d6c03
-select(cities_data, ["city", "state"]) # no !, hence not modified.
+select(city_data, ["city", "state"]) # no !, hence not modified.
 
 # ╔═╡ 581bfc10-0362-11eb-1b29-cfd4320a5130
 md"### delete a row"
 
 # ╔═╡ a583d38b-c309-4b61-8820-159d67bf6874
 # add a bogus row we'll want to remove
-push!(cities_data, ["bogus", "blah", 0, 100.0])
+push!(city_data, ["bogus", "blah", 0, 100.0])
 
 # ╔═╡ b7fb0648-0390-11eb-2dc5-8b6935d2545c
-delete!(cities_data, 6)
+delete!(city_data, 6)
 
 # ╔═╡ b8de77aa-0362-11eb-36d9-1d905442ca13
 md"
@@ -170,10 +170,10 @@ md"
 
 # ╔═╡ 9063e224-053b-46a7-9ff1-bb550fa57964
 # add duplicate row we'll want to remove
-push!(cities_data, ["Berkeley", "CA", 122324, 25.0]) 
+push!(city_data, ["Berkeley", "CA", 122324, 25.0]) 
 
 # ╔═╡ cbf6250c-0362-11eb-365b-d327617f197e
-unique!(cities_data)
+unique!(city_data)
 
 # ╔═╡ 5d7208d4-035b-11eb-00ef-cd70b6cb79d3
 md"
@@ -185,7 +185,7 @@ the columns are `Array`'s. treat the `DataFrame` like a 2D array.
 "
 
 # ╔═╡ 8d4f4ede-035b-11eb-2337-7bdb844389ae
-cities_data[:, "population"]
+city_data[:, "population"]
 
 # ╔═╡ 7daa87e6-035b-11eb-3bb8-ff1bdd95714c
 md"
@@ -193,7 +193,7 @@ md"
 "
 
 # ╔═╡ f735e3ee-035b-11eb-33d1-755a1a9dc0a7
-cities_data[2, :]
+city_data[2, :]
 
 # ╔═╡ 1821e936-035c-11eb-0cb1-014241a2599e
 md"
@@ -201,7 +201,7 @@ md"
 "
 
 # ╔═╡ 1ad35930-035c-11eb-165d-2d70f7b07713
-cities_data[2, :population]
+city_data[2, :population]
 
 # ╔═╡ 9e01dd3a-0362-11eb-3d19-392ec2d06bd6
 md"
@@ -209,7 +209,7 @@ md"
 "
 
 # ╔═╡ a6f99cc8-0362-11eb-1801-2dd5fa96efe1
-unique(cities_data[:, "state"])
+unique(city_data[:, "state"])
 
 # ╔═╡ d663dd98-035a-11eb-156f-ff237a3944b6
 md"
@@ -217,7 +217,7 @@ md"
 "
 
 # ╔═╡ 360eb67a-035b-11eb-2ab3-85adb264a387
-for row in eachrow(cities_data)
+for row in eachrow(city_data)
 	# inside here, row is a ditionary. keys: column names.
 	println(row["city"], " has a population of ", row[:population])
 end
@@ -234,19 +234,19 @@ use the `filter` function. see [the docs](https://dataframes.juliadata.org/stabl
 "
 
 # ╔═╡ 5abb815e-0392-11eb-3576-a7e39e8ac6af
-filter(row -> row["city"] == "Corvallis", cities_data)
+filter(row -> row["city"] == "Corvallis", city_data)
 
 # ╔═╡ 6ca4c6a8-035d-11eb-158c-3380a0cafdaa
 md"**example 2**: query all cities (rows) where the population is less than 500,000."
 
 # ╔═╡ 7dad5c94-035d-11eb-1f7b-2fedd834efaa
-filter(row -> row["population"] < 500000, cities_data)
+filter(row -> row["population"] < 500000, city_data)
 
 # ╔═╡ 7e54ed24-035d-11eb-19e2-4986b3cfcab4
 md"**example 3**: ... and that reside in Oregon."
 
 # ╔═╡ 9879f190-035d-11eb-07c6-55453426c704
-filter(row -> (row["population"] < 500000) && (row["state"] == "OR"), cities_data)
+filter(row -> (row["population"] < 500000) && (row["state"] == "OR"), city_data)
 
 # ╔═╡ 9926bdbc-035d-11eb-1824-438e97d78ab9
 md"## sort rows
@@ -255,7 +255,7 @@ e.g. permute rows so that cities are listed by `:population` in reverse (`rev`) 
 "
 
 # ╔═╡ ab918a54-035d-11eb-27ae-2d70b27460ac
-sort!(cities_data, "population", rev=true)
+sort!(city_data, "population", rev=true)
 
 # ╔═╡ 9ed15498-035d-11eb-1369-53ae1eac0a27
 md"
@@ -263,11 +263,11 @@ md"
 
 the `groupby` command (common in many languages) partitions the rows in the `DataFrame` into multiple `DataFrame`'s such that the rows in each share a common feature (contained in a column). a `GroupedDataFrame` is useful for performing computations on each group of rows in a `DataFrame` separately. see [the docs](https://dataframes.juliadata.org/stable/lib/functions/#DataFrames.groupby).
 
-e.g., group the cities (rows) in `cities_data` by state.
+e.g., group the cities (rows) in `city_data` by state.
 "
 
 # ╔═╡ c1526020-035d-11eb-2d8a-d131aa445738
-gb_state = groupby(cities_data, :state)
+gb_state = groupby(city_data, :state)
 
 # ╔═╡ c106f1b5-946c-44a1-bb63-ad3342adb6aa
 md"the `GroupedDataFrame` works like an array and can be iterated over."
@@ -302,89 +302,99 @@ _example 1_: group by state, take `mean` of the rainfall column.
 "
 
 # ╔═╡ 03a59b6c-035f-11eb-0a39-79c770bf5544
-df_rain = combine(groupby(cities_data, "state"), "rainfall" => mean => "avg_rainfall")
+df_rain = combine(groupby(city_data, "state"), "rainfall" => mean => "avg_rainfall")
 
 # ╔═╡ 02fab01a-e9b8-4a43-aceb-31985194f4c6
 md"
 _example 2_: group by state, determine if `sum` of the rainfall is less than 50. "
 
 # ╔═╡ a5ad8099-c40a-40c4-9197-a47281937537
-combine(groupby(cities_data, "state"), 
+combine(groupby(city_data, "state"), 
 	"rainfall" => (col -> sum(col) < 50.0) => "little_rainfall")
 
 # ╔═╡ 27391eca-ce4b-434f-bba0-13fe0d700298
 md"_example 3_: group by state, compute rainfall per person"
 
 # ╔═╡ a6f0b99d-8b48-4758-89ee-9b2772965cab
-combine(groupby(df_cities, :state),
-	[:population, :rainfall] => 
+combine(groupby(city_data, "state"),
+	["population", "rainfall"] => 
 	  ((pop_col, rain_col) -> sum(rain_col) / sum(pop_col)) => 
-	  :rainfall_per_population
+	  "rainfall_per_population"
 	)
 
 # ╔═╡ 8226dc8e-0362-11eb-17bf-47cae0df2907
-md"#### write a `DataFrame` to a `.csv` file
+md"## I/O with `.csv` files
 
 use the `CSV.jl` package.
 
 CSV = comma separated value
+
+### `DataFrame` to `.csv`
 "
 
 # ╔═╡ 907832ea-0362-11eb-2132-a3abadd4b1ee
 CSV.write("rainfall.csv", df_rain)
 
 # ╔═╡ c711c3f8-0393-11eb-2fbc-77693069c73f
-with_terminal() do
-	run(`cat rainfall.csv`)
-end
+run(`cat rainfall.csv`)
 
 # ╔═╡ 08e91b1c-035f-11eb-05d0-9fe60938a4e3
-md"#### read in a `.csv` file into a `DataFrame`
-use the `CSV.jl` package.
-
-CSV = comma separated value
+md"### `.csv` to `DataFrame`
+first, let's download the `.csv` from Github [here](https://raw.githubusercontent.com/SimonEnsemble/intro_to_data_science_fall_2022/main/data/incomes.csv) into our present working directory (`pwd`).
 "
 
 # ╔═╡ fdab541a-0393-11eb-0318-3390bd75a95d
 pwd() # present working directory to see where CSV looks for files
 
+# ╔═╡ 2604e77f-7ea0-4049-bb17-d7b5d0ca9c29
+readdir()
+
+# ╔═╡ 717b8f72-61ab-431c-9fc8-d518523b4674
+url = "https://raw.githubusercontent.com/SimonEnsemble/intro_to_data_science_fall_2022/main/data/incomes.csv"
+
+# ╔═╡ 2823012b-b80f-4532-a614-539eeba366da
+download(url, "incomes.csv")
+
 # ╔═╡ 1c01557a-035f-11eb-37e8-e9497003725f
-df_incomes = CSV.read(joinpath("..", "..", "data", "incomes.csv"), DataFrame)
+income_data = CSV.read("incomes.csv", DataFrame)
 
 # ╔═╡ 4cf973b8-0361-11eb-1777-cf02396ba052
 md"
-#### joins
-combine the rows of two data frames on a key column.
+## joining two tables of data
+\"join\" = combine the rows of two tables of data on a \"key\" column.
 
 there are [seven types of joins](http://juliadata.github.io/DataFrames.jl/stable/man/joins/). let's illustrate two here.
 
-goal: join information about *cities* from `df_cities` and `df_incomes`. thus the *key* here is `:city` since we aim to combine rows of the two `DataFrames` on the basis of the `:city` column.
+_goal_: join information about cities from `city_data` and `income_data`. 
+the *key* column here is `\"city\"` since we aim to combine rows of the two `DataFrames` whose feature is the same in this column.
 
-subtleties here. 
-* San Francisco is in `df_incomes` but missing from `df_cities`
-* Bend, Eugene, Portland are in `df_cities` but missing from `df_incomes`
+there are two subtleties here:
+* the city of San Francisco appears in `income_data` but is missing from `city_data`
+* the cities of Bend, Eugene, Portland appear in `city_data` but are missing from `income_data`
 `innerjoin` and `outerjoin` are distinguished by how they handle these subtleties.
 
-##### inner join
-only keep rows where the city (the entry in the `on` column) is in _both_ `DataFrames`s
+### inner join
+only keep rows where the value in the \"key\" column appears in _both_ `DataFrames`'s
 (throw out rows that aren't common between the two)
 "
 
 # ╔═╡ 74379f7c-0361-11eb-0192-c59bca513893
-df_ij = innerjoin(df_cities, df_incomes, on=:city)
+data_ij = innerjoin(city_data, income_data, on=:city)
 
 # ╔═╡ 80c12360-0361-11eb-3eb3-eddb35dac4a5
 md"
-##### outer join
+### outer join
 keep all rows, fill entries with `missing` if an attribute is missing in either `DataFrame`.
+
+note the `?` appears in the type under the column name to indicate some entries are `missing`.
 "
 
 # ╔═╡ 02bef8b2-0362-11eb-130f-f99cc7311f5a
-df_oj = outerjoin(df_cities, df_incomes, on=:city)
+data_oj = outerjoin(city_data, income_data, on=:city)
 
 # ╔═╡ 098a5628-0362-11eb-33af-9fc2fbceddba
-md"#### missing values
-Julia has a data type to efficiently handle missing values
+md"## missing values
+Julia has a data type to efficiently handle missing values.
 "
 
 # ╔═╡ 12deee64-0362-11eb-3612-ed369a623583
@@ -397,19 +407,25 @@ typeof(missing)
 md"columns with missing values are arrays of whatever type but `Union`'d with the `Missing` type"
 
 # ╔═╡ 25a8858c-0362-11eb-3405-95aeea8c1338
-typeof(df_oj[:, :state])
+typeof(data_oj[:, "state"])
+
+# ╔═╡ 2f441675-6930-4f97-b7e1-dfc1c25cdbc6
+md"### removing rows with `missing` entries"
+
+# ╔═╡ 86ec1cfe-d75e-4f90-a7e7-ed438517641f
+data_oj
 
 # ╔═╡ 2fb25d0c-0362-11eb-16b3-b75f845f82a9
-md"remove all rows that have a missing attribute"
+md"..._all_ rows with a missing entry."
 
 # ╔═╡ 36ba914e-0362-11eb-0aa7-6fda9f1b4d02
-dropmissing(df_oj)
+dropmissing(data_oj)
 
 # ╔═╡ 38ab5560-0362-11eb-15cb-4595de21d218
-md"remove all rows with a missing state"
+md"...only rows with a `missing` entry in a certain column."
 
 # ╔═╡ 3edf858c-0362-11eb-3b47-5f53c1360718
-dropmissing(df_oj, :state)
+dropmissing(data_oj, "state")
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
 PLUTO_PROJECT_TOML_CONTENTS = """
@@ -861,6 +877,9 @@ version = "17.4.0+0"
 # ╠═c711c3f8-0393-11eb-2fbc-77693069c73f
 # ╟─08e91b1c-035f-11eb-05d0-9fe60938a4e3
 # ╠═fdab541a-0393-11eb-0318-3390bd75a95d
+# ╠═2604e77f-7ea0-4049-bb17-d7b5d0ca9c29
+# ╠═717b8f72-61ab-431c-9fc8-d518523b4674
+# ╠═2823012b-b80f-4532-a614-539eeba366da
 # ╠═1c01557a-035f-11eb-37e8-e9497003725f
 # ╟─4cf973b8-0361-11eb-1777-cf02396ba052
 # ╠═74379f7c-0361-11eb-0192-c59bca513893
@@ -871,6 +890,8 @@ version = "17.4.0+0"
 # ╠═977c25ce-0394-11eb-0076-0955dcfe0ca1
 # ╟─1e41218c-0362-11eb-2ae3-17339b033f7a
 # ╠═25a8858c-0362-11eb-3405-95aeea8c1338
+# ╟─2f441675-6930-4f97-b7e1-dfc1c25cdbc6
+# ╠═86ec1cfe-d75e-4f90-a7e7-ed438517641f
 # ╟─2fb25d0c-0362-11eb-16b3-b75f845f82a9
 # ╠═36ba914e-0362-11eb-0aa7-6fda9f1b4d02
 # ╟─38ab5560-0362-11eb-15cb-4595de21d218
